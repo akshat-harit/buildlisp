9a6c9da1c06bc275f27fa0f7e7af791175924e7c
Akshat Harit <akshat.harit@gmail.com>
2016-11-26 12:52:50 -0800

    Added mpc library Added parsing for numeric operators

--- prompt.c
+++ prompt.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "mpc.h"
 
 #ifdef _WIN32
 #include<string.h>
@@ -33,14 +34,40 @@ void add_history(const char * unused){}
 
 
 int main(int argv, char **argc){
+
+	mpc_parser_t* Number = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr = mpc_new("expr");
+	mpc_parser_t* Lispy= mpc_new("lispy");
+	mpc_result_t r;
+
+	// Initialize grammar
+	mpca_lang(MPCA_LANG_DEFAULT,
+			"					\
+			number : /-?[0-9]+/ ;			\
+			operator : '+' | '-' | '/' | '*' ;	\
+			expr : <number> | '(' <operator> <expr>+ ')' ;\
+			lispy : /^/ <operator> <expr>+ /$/ ;	\
+			",
+			Number, Operator, Expr, Lispy);
+
+	// Version prompt
 	puts("Lispy version 0.0.1");
 	puts("Press Ctrl+c to exit\n");
 
 	while(TRUE){
 		char * input = readline("lispy>");
 		add_history(input);
-		printf("You entered %s\n", input);
+		if (mpc_parse("<stdin>", input, Lispy, &r)) {
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		} else{
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
 		free(input);
-	}	
+	}
+
+	mpc_cleanup(4, Number, Operator, Expr, Lispy);
 	return 0;
 }
