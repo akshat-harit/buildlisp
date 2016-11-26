e9c0edfa5fe8d442afc4180db4cd700aeee3f365
Akshat Harit <akshat.harit@gmail.com>
2016-11-26 11:05:28 -0800

    Added code for windows compatibility

--- prompt.c
+++ prompt.c
@@ -1,14 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#include<editline/readline.h>
-#include<editline/history.h>
+#ifdef _WIN32
+#include<string.h>
 
 #define BUFFER_SIZE 2048
+static char buffer[BUFFER_SIZE];
+
+char * readline(const char * prompt){
+	fputs(prompt);
+	fgets(buffer, BUFFER_SIZE, stdin);
+	char * ret = malloc(strlen(buffer) + 1);
+	if (ret == NULL){
+		printf("\nOut of memory error!\n");
+		exit(1);
+	}
+	else {
+		strcpy(ret, buffer);
+		return ret;
+	} 
+}
+
+void add_history(const char * unused){}
+
+#else
+#include<editline/readline.h>
+#include<editline/history.h>
+#endif
+
 #define TRUE 1
 #define FALSE 0
 
-static char input[BUFFER_SIZE];
 
 int main(int argv, char **argc){
 	puts("Lispy version 0.0.1");
