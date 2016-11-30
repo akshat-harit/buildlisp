92b24c91126534aec2257dafeddcf44cfa1d8f8c
Akshat Harit <akshat.harit@gmail.com>
2016-11-29 22:08:41 -0800

    Added struct and constructors for s-expr

--- prompt.c
+++ prompt.c
@@ -5,10 +5,13 @@
 typedef struct {
 	int type;
 	long num;
-	int err;
+	char* err;
+	char* sym;
+	int count;
+	struct lval** cell;
 } lval;
 
-enum {LVAL_NUM, LVAL_ERR};
+enum {LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR};
 enum {LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM};
 
 
@@ -40,23 +43,74 @@ void add_history(const char * unused){}
 
 #define TRUE 1
 #define FALSE 0
-
-lval lval_num(long x){
-	lval v;
-	v.type = LVAL_NUM;
-	v.num = x;
-	v.err = -1;
+//Create functions
+lval* lval_num(long x){
+	lval* v = malloc(sizeof(lval));
+	v->type = LVAL_NUM;
+	v->num = x;
 	return v;
 }
 
-lval lval_err(int x){
-	lval v;
-	v.type = LVAL_ERR;
-	v.err = x;
-	v.num = 0;
+lval* lval_err(char* m){
+	lval* v = malloc(sizeof(lval));
+	v->type = LVAL_ERR;
+	v->err = malloc(strlen(m)+1);
+	strcpy(v->err, m);
 	return v;
 }
 
+lval* lval_sym(char* s){
+	lval* v = malloc(sizeof(lval));
+	v->type = LVAL_SYM;
+	v->err = malloc(strlen(s)+1);
+	strcpy(v->sym, m);
+	return v;
+}
+
+
+lval* lval_sexpr(void){
+	lval* v = malloc(sizeof(lval));
+	v->type = LVAL_SEXPR;
+	v->count = 0;
+	v->cell = NULL;	
+	return v;
+}
+
+// Delete functions
+void lval_del(lval* v) {
+	
+	switch(v->type){
+		case LVAL_NUM: break;
+		case LVAL_ERR: free(v->err); break;
+		case LVAL_SYM: free(v->sym); break;
+		case LVAL_SEXPR :
+			for(int i =0 ; i<= v->count; i++){
+				lval_del(v->cell[i]);
+				}
+			free(v->cell);
+			break;
+		}
+	free(v);
+}
+
+//Read functions
+lval* lval_read_num(mpc_ast_t* t){
+	errno = 0;
+	long x = strtol(t->contents, NULL, 10);
+	return errno !=ERANGE ? lval_num(x) : lval_err("Invalid Number");
+}
+//TODO
+/*
+lval* lval_read(mpc_ast_t* t){
+	
+	if (strstr(t->tag, "number")) { return lval_read_num(t);}
+	if (strstr(input->tag, "symbol")) { return lval_sym(t->contents);}
+
+	lval* x = NULL;
+	int i = 3;
+
+	}
+*/
 void lval_print(lval v){
 	switch(v.type){
 		case LVAL_NUM: printf("%li", v.num);break;
@@ -115,17 +169,19 @@ lval eval(mpc_ast_t* input){
 int main(int argv, char **argc){
 
 	mpc_parser_t* Number = mpc_new("number");
-	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Symbol = mpc_new("symbol");
 	mpc_parser_t* Expr = mpc_new("expr");
-	mpc_parser_t* Lispy= mpc_new("lispy");
+	mpc_parser_t* Lispy = mpc_new("lispy");
+	mpc_parser_t* Sexpr = mpc_new("sexpr");
 	mpc_result_t r;
 
 	// Initialize grammar
 	mpca_lang(MPCA_LANG_DEFAULT,
 			"					\
 			number : /-?[0-9]+/ ;			\
-			operator : '+' | '-' | '/' | '*' ;	\
-			expr : <number> | '(' <operator> <expr>+ ')' ;\
+			symbol : '+' | '-' | '/' | '*' ;	\
+			sexpr : '(' <expr?* ')' ;		\
+			expr : <number> | <symbol> | <sexpr> ; 	\ 
 			lispy : /^/ <operator> <expr>+ /$/ ;	\
 			",
 			Number, Operator, Expr, Lispy);
@@ -148,7 +204,7 @@ int main(int argv, char **argc){
 		free(input);
 	}
 
-	mpc_cleanup(4, Number, Operator, Expr, Lispy);
+	mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
 	return 0;
 }
 
